44c44823c8b935559158e318d2c4cf4acdf7ca34
Johnny Jonsson <johnny.jonsson@ericsson.com>
2022-11-10 14:13:08 +0100

    nvim: Adapt for 8.0

--- nvim/lua/jj.lua
+++ nvim/lua/jj.lua
@@ -1,5 +1,5 @@
 -- Hook LSP into completion
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp
   .protocol
   .make_client_capabilities())
 
